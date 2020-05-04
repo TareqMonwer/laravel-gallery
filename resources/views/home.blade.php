0fa0635e96c35df0d9c4be29ff6079bf839ae9e7
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-04 17:15:09 +0600

    jquery multiple image upload ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,47 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <form action="{{ route('album.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <input type="text" name="album" id="album" class="form-control"
-                placeholder="Your Album Name">
-                <input type="file" name="image[]" class="form-control" id="image">
-                <input type="file" name="image[]" class="form-control" id="image">
-                <input type="file" name="image[]" class="form-control" id="image">
-                <button class="btn btn-primary mt-2" type="submit">Submit</button>
-            </form>
 
-            @foreach($images as $image)
-                <img src="{{ asset('storage/'.$image->name) }}" alt="" width="200">
-            @endforeach
+            <div class="card p-2">
+                <div class="input-group control-group init-add-more mb-1">
+                    <input type="file" name="image[]" id="image" class="form-control">
+                    <div class="input-group-btn">
+                        <button class="btn btn-success btn-add-more" type="button">
+                            Add
+                        </button>
+                    </div>
+                </div>
+
+                <div class="copy" style="display: none";>
+                    <div class="input-group control-group add-more">
+                        <input type="file" name="image[]" id="image" class="form-control">
+                        <div class="input-group-btn">
+                            <button class="btn btn-danger remove" type="button">
+                                Remove
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
 </div>
 @endsection
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+<script>
+    $(document).ready(function (e) {
+        // Add more button
+        $(".btn-add-more").click(function () {
+            // copy the html inside copy div.
+            const html = $(".copy").html();
+            $(".init-add-more").after(html)
+        });
+
+        // Remove button
+        $("body").on("click", ".remove", function () {
+            $(this).parents(".control-group").remove();
+        });
+    });
+</script>
